9c2c8a4e6707a89c57c8becbe8fcbbfc7fdbc1d6
Anarasty <murloc.ninja16@gmail.com>
2025-06-16 13:24:14 +0300

    experience boxes

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const resumeBtns = document.querySelectorAll('.resume__btn')
 
 resumeBtns.forEach((btn, idx) => {
     btn.addEventListener('click', () => {
+        const resumeDetails = document.querySelectorAll(".resume__detail")
+
         resumeBtns.forEach(btn => {
             btn.classList.remove('active')
         })
         btn.classList.add('active')
+
+        resumeDetails.forEach(detail => {
+            detail.classList.remove('active')
+        })
+        resumeDetails[idx].classList.add('active')
     })
 })
